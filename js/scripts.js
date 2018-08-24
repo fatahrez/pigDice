c4fb0b981e6d0ecc2b39f815215a5d020d788540
Abdulfatah Mohamed <fertahrez@gmail.com>
2018-08-24 15:48:39 +0300

    Add functionality and button for generating a random number

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,20 @@
-var rollRandom = Math.floor(Math.random() * 6) + 1;
-return rollRandom;
+//Backend logic
+  var player1 = "";
+  var player2 = "";
+  var rollRandom = Math.floor(Math.random() * 6) + 1;
+  function Player1 (roll, hold){
+    this.rollRandom = roll;
+    this.holdDice = hold;
+
+  }
+  function Player2 (roll, hold){
+    this.rollRandom = roll;
+    this.holdDice = hold;
+
+  }
+//frontEnd logic
+$(document).ready(function(){
+  $("#rollPlayer1").click(function(){
+    $("#output").text(rollRandom);
+  })
+});
