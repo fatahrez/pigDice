8c760723a2986e1ca56976976697786da1b78f84
Abdulfatah Mohamed <fertahrez@gmail.com>
2018-08-26 09:57:35 +0300

    add to backend code for hold and crown winner

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,59 @@
 //Backend logic
+
+//create variables to store the players
   var player1 = "";
   var player2 = "";
-  var rollRandom = Math.floor(Math.random() * 6) + 1;
-  function Player1 (roll, hold){
-    this.rollRandom = roll;
-    this.holdDice = hold;
 
+//Roll dice with random number
+  var rollRandom = function() {
+    Math.floor(Math.random() * 6) + 1;
   }
-  function Player2 (roll, hold){
-    this.rollRandom = roll;
-    this.holdDice = hold;
 
+  //create constructor method for players
+  function Player (throw){
+    this.roll = 0;
+    this.currentScore = 0;
+    this.score= 0;
+    this.throw = throw;
+    this.playerName = "";
   }
+
+//if player gets a score of 1
+Player.prototype.scoreone = function () {
+  if (this.roll === 1) {
+    this.currentScore = 0;
+    alert("You scored nothing." + this.playerName +"Your turn is over pass the dice");
+  } else {
+    this.currentScore += this.roll;
+  }
+}
+
+//prototype for hold
+Player.prototype.hold = function () {
+  this.score += this.currentScore;
+  this.currentScore = 0;
+  alert(this.playerName + ", your turn is over pass the game");
+}
+
+//crown the winner if someone reaches 100
+Player.prototype.crownWinner = function (){
+  if (this.score >= 100){
+    alert(this.playerName + "Congratulations, You have won")
+  }
+}
+
+Player.prot
 //frontEnd logic
 $(document).ready(function(){
-  $("#rollPlayer1").click(function(){
+  //starts game by showing the rolling space and creating two players
+  $("button#playGame").click(function(){
+    player1 = new Player(true);
+    player2 = new Player(false);
+
+
+  })
+
+  $("button#rollPlayer1").click(function(){
     $("#output").text(rollRandom);
   })
 });
