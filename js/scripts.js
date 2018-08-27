fd1e7b4bbb37c699d5c8c82296a71012dbe4318a
Abdulfatah Mohamed <fertahrez@gmail.com>
2018-08-27 23:15:03 +0300

    add roll for each player turn

--- js/scripts.js
+++ js/scripts.js
@@ -31,10 +31,35 @@ var changePlayer = function() {
   if (player1.isActive === true) {
     player1.isActive = false;
     player2.isActive = true;
-    turnScoreReset();
+    scoreReset();
   } else {
     player2.isActive = false;
     player1.isActive = true;
-    turnScoreReset();
+    scoreReset();
   }
+}
+
+var scoreReset = function() {
+  player1.turnScore = 0;
+  player2.turnScore = 0;
+}
+
+var playerToRoll = function() {
+  if (player1.isActive === true) {
+    playerRoll(player1);
+  } else {
+    playerRoll(player2);
+  }
+}
+
+var playerRoll = function(player) {
+  var roll = randomDie;
+  if(roll === 1){
+    player.turnScore = 0;
+    switchPlayer();
+  } else {
+    player.turnScore += roll;
+  }
+  checkForEndOfGame();
+  return roll;
 }
